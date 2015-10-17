7b677d940350375a9a8e00cdddefa7c625ff56e0
Jonathan Islas <islas.27@gmail.com>
2015-10-16 20:30:20 -0600

    Boceto de pagina totalente dinamica
    --Se inicia el boceto para una pagina totalmente dinamica

---
+++ prueba.php
@@ -0,0 +1,75 @@
+<?php $var =
+'{value: '.$value[0].',
+ color:"#367F7F",
+ highlight: "#56CCCB",
+ label: "'.$name[0].'"},
+ 
+ {value: '.$value[1].',
+ color: "#285C7F",
+ highlight: "#4094CC",
+ label: "'.$name[1].'"},
+ 
+ {value: '.$value[2].',
+ color: "#357F47",
+ highlight: "#55CC72",
+ label: "'.$name[2].'"},
+ 
+ {value: '.$value[3].',
+ color: "#5E7F31",
+ highlight: "#96CC4F",
+ label: "'.$name[3].'"},
+ 
+ {value: '.$value[4].',
+ color: "#CCC976",
+ highlight: "#FFFB94",
+ label: "'.$name[4].'"},
+ 
+ {value: '.$value[5].',
+ color: "#B6CC2F",
+ highlight: "#E4FF3B",
+ label: "'.$name[5].'"},
+ 
+ {value: '.$value[6].',
+ color: "#CC913C",
+ highlight: "#FFB54B",
+ label: "'.$name[6].'"},
+ 
+ {value: '.$value[7].',
+ color: "#CC6E5A",
+ highlight: "#FF8971",
+ label: "'.$name[7].'"}
+ 
+ {value: '.$value[8].',
+ color: "#CC3F74",
+ highlight: "#FF4F91",
+ label: "'.$name[8].'"},
+ 
+ {value: '.$value[9].',
+ color: "#CC9699",
+ highlight: "#FFBBBF",
+ label: "'.$name[9].'"},
+ 
+ {value: '.$value[10].',
+ color: "#9684CC",
+ highlight: "#BBA6FF",
+ label: "'.$name[10].'"},
+ 
+ {value: '.$value[11].',
+ color: "#6567CC",
+ highlight: "#7E81FF",
+ label: "'.$name[11].'"},
+ 
+ {value: '.$value[12].',
+ color: "#4687CC",
+ highlight: "#58A9FF",
+ label: "'.$name[12].'"},
+ 
+ {value: '.$value[13].',
+ color: "#36B7CC",
+ highlight: "#43E5FF",
+ label: "'.$name[13].'"},
+ 
+ {value: '.$value[14].',
+ color: "#25CC8B",
+ highlight: "#2FFFAE",
+ label: "'.$name[14].'"}';
--- variablesVistas.php
+++ variablesVistas.php
@@ -62,37 +62,79 @@
         <script>
             var doughnutData = [
                     {value: '.$value[0].',
-                        color:"#F7464A",
-                        highlight: "#FF5A5E",
-                        label: "'.$name[0].'"},
-                    {value: '.$value[1].',
-                        color: "#46BFBD",
-                        highlight: "#5AD3D1",
-                        label: "'.$name[1].'"},
-                    {value: '.$value[2].',
-                        color: "#FDB45C",
-                        highlight: "#FFC870",
-                        label: "'.$name[2].'"},
-                    {value: '.$value[3].',
-                        color: "#949FB1",
-                        highlight: "#A8B3C5",
-                        label: "'.$name[3].'"},
-                    {value: '.$value[4].',
-                        color: "#F7BE81",
-                        highlight: "#F5D0A9",
-                        label: "'.$name[4].'"},
-                    {value: '.$value[5].',
-                        color: "#FA8258",
-                        highlight: "#F79F81",
-                        label: "'.$name[5].'"},
-                    {value: '.$value[6].',
-                        color: "#F7819F",
-                        highlight: "#F5A9BC",
-                        label: "'.$name[6].'"},
-                    {value: '.$value[7].',
-                        color: "#4D5360",
-                        highlight: "#616774",
-                        label: "'.$name[7].'"}
+ color:"#367F7F",
+ highlight: "#56CCCB",
+ label: "'.$name[0].'"},
+ 
+ {value: '.$value[1].',
+ color: "#285C7F",
+ highlight: "#4094CC",
+ label: "'.$name[1].'"},
+ 
+ {value: '.$value[2].',
+ color: "#357F47",
+ highlight: "#55CC72",
+ label: "'.$name[2].'"},
+ 
+ {value: '.$value[3].',
+ color: "#5E7F31",
+ highlight: "#96CC4F",
+ label: "'.$name[3].'"},
+ 
+ {value: '.$value[4].',
+ color: "#CCC976",
+ highlight: "#FFFB94",
+ label: "'.$name[4].'"},
+ 
+ {value: '.$value[5].',
+ color: "#B6CC2F",
+ highlight: "#E4FF3B",
+ label: "'.$name[5].'"},
+ 
+ {value: '.$value[6].',
+ color: "#CC913C",
+ highlight: "#FFB54B",
+ label: "'.$name[6].'"},
+ 
+ {value: '.$value[7].',
+ color: "#CC6E5A",
+ highlight: "#FF8971",
+ label: "'.$name[7].'"},
+ 
+ {value: '.$value[8].',
+ color: "#CC3F74",
+ highlight: "#FF4F91",
+ label: "'.$name[8].'"},
+ 
+ {value: '.$value[9].',
+ color: "#CC9699",
+ highlight: "#FFBBBF",
+ label: "'.$name[9].'"},
+ 
+ {value: '.$value[10].',
+ color: "#9684CC",
+ highlight: "#BBA6FF",
+ label: "'.$name[10].'"},
+ 
+ {value: '.$value[11].',
+ color: "#6567CC",
+ highlight: "#7E81FF",
+ label: "'.$name[11].'"},
+ 
+ {value: '.$value[12].',
+ color: "#4687CC",
+ highlight: "#58A9FF",
+ label: "'.$name[12].'"},
+ 
+ {value: '.$value[13].',
+ color: "#36B7CC",
+ highlight: "#43E5FF",
+ label: "'.$name[13].'"},
+ 
+ {value: '.$value[14].',
+ color: "#25CC8B",
+ highlight: "#2FFFAE",
+ label: "'.$name[14].'"}
 
                 ];
         </script>';
@@ -108,37 +150,79 @@
         <script>
             var doughnutData2 = [
                     {value: '.$value[0].',
-                        color:"#F7464A",
-                        highlight: "#FF5A5E",
-                        label: "'.$name[0].'"},
-                    {value: '.$value[1].',
-                        color: "#46BFBD",
-                        highlight: "#5AD3D1",
-                        label: "'.$name[1].'"},
-                    {value: '.$value[2].',
-                        color: "#FDB45C",
-                        highlight: "#FFC870",
-                        label: "'.$name[2].'"},
-                    {value: '.$value[3].',
-                        color: "#949FB1",
-                        highlight: "#A8B3C5",
-                        label: "'.$name[3].'"},
-                    {value: '.$value[4].',
-                        color: "#F7BE81",
-                        highlight: "#F5D0A9",
-                        label: "'.$name[4].'"},
-                    {value: '.$value[5].',
-                        color: "#FA8258",
-                        highlight: "#F79F81",
-                        label: "'.$name[5].'"},
-                    {value: '.$value[6].',
-                        color: "#F7819F",
-                        highlight: "#F5A9BC",
-                        label: "'.$name[6].'"},
-                    {value: '.$value[7].',
-                        color: "#4D5360",
-                        highlight: "#616774",
-                        label: "'.$name[7].'"}
+ color:"#367F7F",
+ highlight: "#56CCCB",
+ label: "'.$name[0].'"},
+ 
+ {value: '.$value[1].',
+ color: "#285C7F",
+ highlight: "#4094CC",
+ label: "'.$name[1].'"},
+ 
+ {value: '.$value[2].',
+ color: "#357F47",
+ highlight: "#55CC72",
+ label: "'.$name[2].'"},
+ 
+ {value: '.$value[3].',
+ color: "#5E7F31",
+ highlight: "#96CC4F",
+ label: "'.$name[3].'"},
+ 
+ {value: '.$value[4].',
+ color: "#CCC976",
+ highlight: "#FFFB94",
+ label: "'.$name[4].'"},
+ 
+ {value: '.$value[5].',
+ color: "#B6CC2F",
+ highlight: "#E4FF3B",
+ label: "'.$name[5].'"},
+ 
+ {value: '.$value[6].',
+ color: "#CC913C",
+ highlight: "#FFB54B",
+ label: "'.$name[6].'"},
+ 
+ {value: '.$value[7].',
+ color: "#CC6E5A",
+ highlight: "#FF8971",
+ label: "'.$name[7].'"},
+ 
+ {value: '.$value[8].',
+ color: "#CC3F74",
+ highlight: "#FF4F91",
+ label: "'.$name[8].'"},
+ 
+ {value: '.$value[9].',
+ color: "#CC9699",
+ highlight: "#FFBBBF",
+ label: "'.$name[9].'"},
+ 
+ {value: '.$value[10].',
+ color: "#9684CC",
+ highlight: "#BBA6FF",
+ label: "'.$name[10].'"},
+ 
+ {value: '.$value[11].',
+ color: "#6567CC",
+ highlight: "#7E81FF",
+ label: "'.$name[11].'"},
+ 
+ {value: '.$value[12].',
+ color: "#4687CC",
+ highlight: "#58A9FF",
+ label: "'.$name[12].'"},
+ 
+ {value: '.$value[13].',
+ color: "#36B7CC",
+ highlight: "#43E5FF",
+ label: "'.$name[13].'"},
+ 
+ {value: '.$value[14].',
+ color: "#25CC8B",
+ highlight: "#2FFFAE",
+ label: "'.$name[14].'"}
 
                 ];
 
